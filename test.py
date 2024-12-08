77f84a1d5d6623ac02198e6957c90839e5928d29
YAO <yaojiaye2022@gmail.com>
2024-12-09 01:07:10 +0900

    第二次提交

---
+++ test.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    """Add two numbers together.
+ 
+    Args:
+        a (int): First number to add.
+        b (int): Second number to add.
+ 
+    Returns:
+        int: Sum of a and b.
+    """
+    return a + b
+
+str = "  Hello Worl d  ".strip().split(" ")
+print(str)
